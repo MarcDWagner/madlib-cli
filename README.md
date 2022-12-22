b2291861b7c4ebd7e4730454e627364b57a542fb
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-22 07:24:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lab - Class 01
+
+## Project: Mad Lib
+
+### Author: Marc Wagner
+
+### Links and Resources
+
+### Setup
+
+### Initialize:
+
+ python madlib.py
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,6 @@
+# read into dark_and_stormy_night_template
+
+
+with open(assets/dark_and_stormy_night_template.txt) as f:
+    contents = f.read()
+    print(contents)
