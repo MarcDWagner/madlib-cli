4895eaa85fb135bc486e2608ded685e32a75f8ed
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-22 08:54:56 -0700

    completed set up, added text to template, def read template, tested and failed read template, will re-engage at a later time

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,6 +1,17 @@
+from textwrap import dedent
+
+# def welcome():
+#     print(dedent("""
+#     Welcome to Madlib!
+#     During the madlib process, you will be prompted to enter a part of speech (noun, adjective, verb, etc..), once all prompts have been filled a message will be returned using those parts of speech you input in order to create a sentence.
+#     """))
+
+# welcome()
 # read into dark_and_stormy_night_template
+def read_template(self):
+    with open("assets/dark_and_stormy_night_template.txt") as f:
+        contents = f.read()
+        print(contents)
 
 
-with open(assets/dark_and_stormy_night_template.txt) as f:
-    contents = f.read()
-    print(contents)
+# def parse_template():
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,5 +1,6 @@
 import pytest
-from madlib_cli.madlib import read_template, parse_template, merge
+from madlib_cli.madlib import read_template\
+    # , parse_template, merge
 
 
 def test_read_template_returns_stripped_string():
